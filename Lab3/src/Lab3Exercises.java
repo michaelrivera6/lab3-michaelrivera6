806705ce25541e60e826919a7ddbbc0759bcc16f
michaelrivera6 <michael.rivera6@upr.edu>
2017-11-25 15:28:44 -0400

    Changed the size and location of the frame window.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Michael ");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) screenSize.getWidth(); 
+		int height = (int) screenSize.getHeight();
+		myFrame.setLocation((width/2) -100, (height/2) -100); 
 	}
 }
