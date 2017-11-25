7aa0fe47aa4993c56d314bff019e31e0af9df103
michaelrivera6 <michael.rivera6@upr.edu>
2017-11-25 15:38:03 -0400

    Changed the size, name and location of the window frame.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Michael ");
+		JFrame myFrame = new JFrame("Michael");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
